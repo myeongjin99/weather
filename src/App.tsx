c6d1b92ab68302355dcecc890a1423f38eb4d594
seo myeong jin <seomyeongjin99@gmail.com>
2024-03-12 22:25:12 +0900

    <Feat : 날씨 api 연결>

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainPage from "./pages/MainPage";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,30 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import axios from "axios";
+import sun from "../assets/images/cloudy.jpg";
+
+// 전역으로 QueryClient 객체 생성
+const queryClient = new QueryClient();
 
 const MainPage = () => {
+  const [weather, setWeather] = useState(null);
+
+  const getCurrentLocation = () => {
+    // 현재 위치 가져오기
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log("현재 위치", lat, lon);
+      getWeather(lat, lon);
+    });
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  const getWeather = async (lat: any, lon: any) => {
+    const weatherKey = process.env.REACT_APP_WEATHERKEY;
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKey}`
+    );
+    console.log("날씨 데이터:", response.data);
+    setWeather(response.data);
+    return response.data;
+  };
+
   return (
-    <MainPageContainer>
-      <Location>서울특별시</Location>
-      <Temperature>7 °</Temperature>
-      <Wrapper>
-        <TemperatureContainer>
-          <MTemp>최고 : 8°</MTemp>
-          <MTemp>최저 : 7°</MTemp>
-        </TemperatureContainer>
-        <Humidity>42%</Humidity>
-      </Wrapper>
-      <DayOfWeatherContainer>
-        <DayWeather>
-          월<div>10° 0°</div>
-        </DayWeather>
-        <DayWeather>
-          화<div>13° 4°</div>
-        </DayWeather>
-        <DayWeather>
-          수<div>11° 2°</div>
-        </DayWeather>
-      </DayOfWeatherContainer>
-    </MainPageContainer>
+    <QueryClientProvider client={queryClient}>
+      <MainPageContainer>
+        {weather && (
+          <>
+            <Location>{weather.name}</Location>
+            <Temperature>{Math.round(weather.main.temp - 273.15)}°</Temperature>
+            <Wrapper>
+              <TemperatureContainer>
+                <MTemp>
+                  최고 : {Math.round(weather.main.temp_max - 273.15)}°
+                </MTemp>
+                <MTemp>
+                  최저 : {Math.round(weather.main.temp_min - 273.15)}°
+                </MTemp>
+              </TemperatureContainer>
+              {/* <img src={rainy} alt="" /> */}
+              <Humidity>{weather.main.humidity}%</Humidity>
+            </Wrapper>
+          </>
+        )}
+        <DayOfWeatherContainer>
+          <DayWeather>
+            월<div>{}</div>
+          </DayWeather>
+          <DayWeather>
+            화<div>{/* 날씨 데이터 적용 */}</div>
+          </DayWeather>
+          <DayWeather>
+            수<div>{/* 날씨 데이터 적용 */}</div>
+          </DayWeather>
+        </DayOfWeatherContainer>
+      </MainPageContainer>
+    </QueryClientProvider>
   );
 };
 
@@ -33,6 +76,8 @@ const MainPageContainer = styled.div`
   height: 100vh;
   border: 1px solid black;
   margin: 0 auto;
+  background-image: url(${sun});
+  opacity: 70%;
 `;
 
 const Location = styled.div`
@@ -41,6 +86,7 @@ const Location = styled.div`
   display: flex;
   left: 0;
   margin: 25px;
+  color: white;
 `;
 
 const Temperature = styled.div`
@@ -49,6 +95,7 @@ const Temperature = styled.div`
   display: flex;
   left: 0;
   margin: 25px;
+  color: white;
 `;
 
 const Wrapper = styled.div`
@@ -59,7 +106,7 @@ const Wrapper = styled.div`
 const TemperatureContainer = styled.div`
   display: flex;
 
-  color: gray;
+  color: #e4e4e4;
 `;
 
 const MTemp = styled.div`
@@ -70,12 +117,14 @@ const MTemp = styled.div`
 const Humidity = styled.div`
   font-family: "GongGothicMedium";
   font-size: 20px;
+  color: #e4e4e4;
 `;
 
 const DayOfWeatherContainer = styled.div`
   width: 450px;
   height: 200px;
-  border: 1px solid black;
+  background-color: #e4e4e4;
+  opacity: 80%;
   border-radius: 20px;
   margin: 0 auto;
   display: flex;
