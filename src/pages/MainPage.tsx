cea1e72b1d401590d5cfae05ece71c0439a98a56
seo myeong jin <seomyeongjin99@gmail.com>
2024-03-22 14:27:44 +0900

    <Feat : 날씨 아이콘 불러오기>

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import axios from "axios";
 import sun from "../assets/images/sun.jpg";
+import { IoWaterOutline } from "react-icons/io5";
 
 // 전역으로 QueryClient 객체 생성
 const queryClient = new QueryClient();
@@ -24,14 +25,27 @@ const MainPage = () => {
     getCurrentLocation();
   }, []);
 
-  const getWeather = async (lat: any, lon: any) => {
-    const weatherKey = process.env.REACT_APP_WEATHERKEY;
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKey}`
-    );
-    console.log("날씨 데이터:", response.data);
-    setWeather(response.data);
-    return response.data;
+  const getWeather = async (lat: number, lon: number) => {
+    try {
+      const weatherKey = process.env.REACT_APP_WEATHERKEY;
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKey}`
+      );
+      console.log("날씨 데이터:", response.data);
+      setWeather(response.data);
+      return response.data;
+
+      // 날씨 아이콘을 불러오기
+      const weatherIcon = response.data.weather[0].icon;
+      const weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}.png`;
+      console.log("날씨 아이콘 URL:", weatherIconUrl);
+
+      // 여기서 날씨 아이콘을 사용하거나, 상태로 설정하거나, 렌더링에 적용할 수 있습니다.
+      return { weatherData: response.data, weatherIconUrl };
+    } catch (error) {
+      console.error("날씨 데이터를 가져오는 데 실패했습니다.", error);
+      throw error; // 에러를 호출자에게 전파할 수도 있습니다.
+    }
   };
 
   return (
@@ -41,6 +55,7 @@ const MainPage = () => {
           <>
             <Location>{weather.name}</Location>
             <Temperature>{Math.round(weather.main.temp - 273.15)}°</Temperature>
+
             <Wrapper>
               <TemperatureContainer>
                 <MTemp>
@@ -50,21 +65,30 @@ const MainPage = () => {
                   최저 : {Math.round(weather.main.temp_min - 273.15)}°
                 </MTemp>
               </TemperatureContainer>
-              {/* <img src={rainy} alt="" /> */}
-              <Humidity>{weather.main.humidity}%</Humidity>
+              <HumidityContainer>
+                <IoWaterOutline
+                  style={{
+                    color: "white",
+                    width: "20px",
+                    height: "20px",
+                    marginRight: "5px",
+                  }}
+                />
+                <Humidity>{weather.main.humidity}%</Humidity>
+              </HumidityContainer>
             </Wrapper>
           </>
         )}
         <DayOfWeatherContainer>
-          <DayWeather>
-            월<div>{}</div>
-          </DayWeather>
-          <DayWeather>
-            화<div>{/* 날씨 데이터 적용 */}</div>
-          </DayWeather>
-          <DayWeather>
-            수<div>{/* 날씨 데이터 적용 */}</div>
-          </DayWeather>
+          {weather?.weather && (
+            <IconContainer>
+              <Icon
+                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                alt="Weather Icon"
+              />
+            </IconContainer>
+          )}
+          <DayWeather> 오늘 뭐 입을까?</DayWeather>
         </DayOfWeatherContainer>
       </MainPageContainer>
     </QueryClientProvider>
@@ -105,13 +129,17 @@ const Wrapper = styled.div`
 `;
 const TemperatureContainer = styled.div`
   display: flex;
-
   color: #e4e4e4;
 `;
 
 const MTemp = styled.div`
   font-family: "GongGothicMedium";
   font-size: 20px;
+`;
+
+const HumidityContainer = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const Humidity = styled.div`
@@ -124,13 +152,20 @@ const DayOfWeatherContainer = styled.div`
   width: 450px;
   height: 200px;
   background-color: #e4e4e4;
-  opacity: 80%;
+  opacity: 90%;
   border-radius: 20px;
   margin: 0 auto;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 20%;
+`;
+
+const IconContainer = styled.div``;
+
+const Icon = styled.img`
+  width: 100px; /* 아이콘 크기에 따라 조절하세요 */
+  height: 100px; /* 아이콘 크기에 따라 조절하세요 */
 `;
 
 const DayWeather = styled.div`
